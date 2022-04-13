4f5bf10ae1ba649a488f757df1729d92c2e7fa65
WDPraise <praisemman777@gmail.com>
2022-04-13 23:24:51 +0100

    variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,28 @@
+es (20 sloc)  255 Bytes
+   
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ *
+ * Description: print single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
