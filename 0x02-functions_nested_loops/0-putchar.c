b0e78030ab0de2c6e690e4f59afb1fef8f451711
WDPraise <praisemman777@gmail.com>
2022-04-14 07:40:58 +0100

    c functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - check description
+ * Description: Prints _putchar and then new line
+ * Return: Always returns (0)
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	char *word = "_putchar";
+
+	while (*word != '\0')
+	{
+		_putchar(*word);
+		word++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
