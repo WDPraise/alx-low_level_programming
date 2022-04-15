903185883c557d6f3e6cab60a8d4f131cc05cb58
WDPraise <praisemman777@gmail.com>
2022-04-15 18:08:27 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,13 @@
 
 void print_alphabet(void)
 {
-	char letter = 'a';
+	char alphabet;
 
-	while (letter <= 'z')
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		_putchar(letter);
-		letter++;
+		_putchar(alphabet);
 	}
+
 	_putchar('\n');
+
 }
