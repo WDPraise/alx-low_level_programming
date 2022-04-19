483fd30b34ea53ab89d2ec2b2fef6c4a63409d66
WDPraise <101477027+WDPraise@users.noreply.github.com>
2022-04-19 05:32:38 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,24 +1,34 @@
+
 #include <stdio.h>
 
 /**
- * main - print sum of multiples of 3 or 5 below 1024
- * Return: 0
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
-	int n;
-	int sum;
+	long int sum;
+	long int first, second;
+	int count;
 
 	sum = 0;
+	first = 0;
+	second = 1;
+	count = 0;
 
-	for (n = 0; n < 1024; n++)
+	while (count < 49)
 	{
-		if ((n % 3 == 0) || (n % 5 == 0))
-		{
-			sum += n;
-		}
+		sum = first + second;
+		printf("%li, ", sum);
+		first = second;
+		second = sum;
+
+		count++;
 	}
-	printf("%d\n", sum);
+	sum = first + second;
+	printf("%li\n", sum);
 
 	return (0);
 }
