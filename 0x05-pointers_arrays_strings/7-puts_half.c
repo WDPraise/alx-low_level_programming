33bc191b17432d7580168bad130f411bf9212142
WDPraise <praisemman777@gmail.com>
2022-04-20 22:48:04 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,8 +15,8 @@ void puts_half(char *str)
 		length++;
 	n = (length / 2);
 
-	if ((length % 2) ==1)
-		n = ((length + 1) /2);
+	if ((length % 2) == 1)
+		n = ((length + 1) / 2);
 
 	for (i = n; str[i] != '\0'; i++)
 		_putchar(str[i]);
