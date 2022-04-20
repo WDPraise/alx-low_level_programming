6df335faaf10136bcc25cd87f0ddf1a15a66f8ba
WDPraise <praisemman777@gmail.com>
2022-04-20 13:46:00 +0100

    pointers and arrays

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,9 +2,14 @@
 #include <stdio.h>
 
 /**
+<<<<<<< Updated upstream
  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0.
+=======
+ * main - print sum of multiples of 3 or 5 below 1024
+ * Return: 0
+>>>>>>> Stashed changes
  */
 
 int main(void)
