8ebaa8b67e515c764fad83c8075e75bda3a7b6ed
WDPraise <101477027+WDPraise@users.noreply.github.com>
2022-04-20 16:42:35 +0100

    Update 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print every character of string
+ * starting with first character, then new line.
+ * @str: string to be printed
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
