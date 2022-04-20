218062d8424b09ca9ca295de297c821d70693ffc
WDPraise <praisemman777@gmail.com>
2022-04-20 15:13:12 -0700

    new commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 
 /**
- * print_array - print an array.
- * @a: the array to print.
+ * print_array - print an array
+ * @a: the array to print
  * @n: array's length
  */
-
 void print_array(int *a, int n)
 {
 	int i;
@@ -14,7 +13,7 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i < n - 1)
+		if (i < n -1)
 		{
 			printf(", ");
 		}
