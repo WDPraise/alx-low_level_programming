3b61598314c4531f4afaa6cea105b2b7a86d932f
WDPraise <praisemman777@gmail.com>
2022-04-30 08:54:44 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,14 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] != '\0')
+	if (*s != '\0')
 	{
-		_putchar(s[i]);
-		_puts_recursion(s + 1);
+		_putchar(*(s + 0));
+		_puts_recursion((s + 1));
 	}
 	else
+	{
 		_putchar('\n');
+		return;
+	}
 }
